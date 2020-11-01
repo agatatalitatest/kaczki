cb37d091621537460ebed8fdb589db8542b5c250
agatatalita <agatamonikaandrzejewska@gmail.com>
2020-11-01 17:56:15 +0100

    Updates

---
+++ precache-manifest.43c33433fb2b0cdb126632bcb433021c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b55f3e4c18bb8a662cda8db621d8fc22",
+    "url": "/kaczki/index.html"
+  },
+  {
+    "revision": "05a258722831f56e6a10",
+    "url": "/kaczki/static/css/main.a5e07925.chunk.css"
+  },
+  {
+    "revision": "9e2141b2209290bec7f6",
+    "url": "/kaczki/static/js/2.c1e4db46.chunk.js"
+  },
+  {
+    "revision": "2954b1568600ec1d7b70d079ce9ac1a4",
+    "url": "/kaczki/static/js/2.c1e4db46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05a258722831f56e6a10",
+    "url": "/kaczki/static/js/main.38291c5b.chunk.js"
+  },
+  {
+    "revision": "527717ef84045141bfb5",
+    "url": "/kaczki/static/js/runtime-main.1f3ae335.js"
+  },
+  {
+    "revision": "b03a6723662e2c57f0036cf0898026ce",
+    "url": "/kaczki/static/media/loader.b03a6723.svg"
+  }
+]);
